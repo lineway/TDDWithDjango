12760c3bb0752fe297df247577e2491bcf587aba
张一铭 <zhangyiming07@163.com>
2017-09-17 23:42:50 +0800

    功能测试检查是否可以输入一个to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -10,9 +10,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
     
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # 检测标题和头部是否包含“To-Do”这个词语
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 在表单中输入待办事项
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # 页面刷新，显示一个新的文本框，可以输入其他待办事项
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
